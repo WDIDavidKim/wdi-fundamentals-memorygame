988d98f22e5e5ae50db4bd825ecca7b3838f1d01
David Kim <MacPro@Davids-MacBook-Pro.local>
2016-06-23 14:43:25 -0700

    updated java

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,9 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardOne === cardThree) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, please try again.')
+}
